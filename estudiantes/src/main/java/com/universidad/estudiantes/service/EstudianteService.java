b7eb76338586464337aad897d24b97eae96abe2d
WillaimPGallardo <willijuan2011@hotmail.com>
2026-04-20 13:19:28 -0500

    COMMIT 3
    git add .
    git commit -m "Implementación lógica de negocio con transacciones"

--- estudiantes/src/main/java/com/universidad/estudiantes/service/EstudianteService.java
+++ estudiantes/src/main/java/com/universidad/estudiantes/service/EstudianteService.java
@@ -1,5 +1,37 @@
 package com.universidad.estudiantes.service;
 
+import com.universidad.estudiantes.model.Estudiante;
+import com.universidad.estudiantes.repository.EstudianteRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class EstudianteService {
-    
+
+    private final EstudianteRepository repo;
+
+    public EstudianteService(EstudianteRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Estudiante> listarTodos() {
+        return repo.findAll();
+    }
+
+    public Estudiante buscarPorId(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("No encontrado"));
+    }
+
+    @Transactional
+    public Estudiante guardar(Estudiante e) {
+        return repo.save(e);
+    }
+
+    @Transactional
+    public void eliminar(Long id) {
+        repo.deleteById(id);
+    }
 }
